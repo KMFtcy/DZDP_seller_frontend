557811273073949e591e1772990240c6db45d4c4
kmftang <kmftangchaoyang@gmail.com>
2024-03-24 14:46:11 +0000

    udpate backend interface

--- public/config.js
+++ public/config.js
@@ -3,16 +3,16 @@ var BASE = {
    * @description api请求基础路径
    */
   API_DEV: {
-    common: "https://common-api.pickmall.cn",
-    buyer: "https://buyer-api.pickmall.cn",
-    seller: "https://store-api.pickmall.cn",
-    manager: "https://admin-api.pickmall.cn",
+    common: "http://120.77.9.30:18000",
+    buyer: "http://120.77.9.30:18000",
+    seller: "http://120.77.9.30:18000",
+    manager: "http://120.77.9.30:18000"
   },
   API_PROD: {
-    common: "https://common-api.pickmall.cn",
-    buyer: "https://buyer-api.pickmall.cn",
-    seller: "https://store-api.pickmall.cn",
-    manager: "https://admin-api.pickmall.cn",
+    common: "http://120.77.9.30:18000",
+    buyer: "http://120.77.9.30:18000",
+    seller: "http://120.77.9.30:18000",
+    manager: "http://120.77.9.30:18000"
   },
   /**
    * @description // 跳转买家端地址 pc端
